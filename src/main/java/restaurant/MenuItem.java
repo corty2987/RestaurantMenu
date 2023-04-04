5de4ad21d23d10418801772dceccd4249fd6bc40
corty2987 <cortyblackwell@gmail.com>
2023-04-03 19:27:44 -0500

    Menu and MenuItem classes finished

--- src/main/java/restaurant/MenuItem.java
+++ src/main/java/restaurant/MenuItem.java
@@ -9,17 +9,66 @@ public class MenuItem {
     private double price;
     private String category;
     private Date dateAdded;
+    private Boolean isNew;
+
 
     //Define constructor to take values for first four fields as parameters
     //initialize dateAdded to the current date
 
-    public MenuItem(String name, String description, double price, String category) {
+    public MenuItem(String name, String description, double price, String category, Boolean isNew) {
         this.name = name;
         this.description = description;
         this.price = price;
         this.category = category;
+        this.isNew = isNew;
         this.dateAdded = new Date();
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public Boolean getNew() {
+        return isNew;
+    }
+
+    public void setNew(Boolean aNew) {
+        isNew = aNew;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
 }
